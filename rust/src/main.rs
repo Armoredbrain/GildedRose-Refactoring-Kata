9bb31eb3f534bda9d2926a378df8447e35679b71
benjamin <benjamin.jaud@septeo.com>
2025-03-20 15:50:13 +0100

    test(log) - add a diff mechanism to facilitate refactoring life cycle

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,13 @@
 mod gildedrose;
 
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
+
 use gildedrose::{GildedRose, Item};
+
+use similar::{ChangeTag, TextDiff};
 
 fn main() {
     let items = vec![
@@ -17,14 +24,48 @@ fn main() {
     ];
     let mut rose = GildedRose::new(items);
 
+    let mut text = String::new();
+
     println!("OMGHAI!");
     for i in 0..=30 {
         println!("-------- day {} --------", i);
+        text.push_str(&format!("-------- day {} -------- \n", i));
         println!("name, sellIn, quality");
+        text.push_str("name, sellIn, quality \n");
         for item in &rose.items {
             println!("{}", item);
+            text.push_str(&format!("{} \n", item));
         }
         println!();
         rose.update_quality();
+    }
+
+    let mut result = File::create("src/test_contender").unwrap();
+    let _echo = result.write_all(text.as_bytes());
+
+    let mut original_file = File::open("src/test_original").unwrap();
+    let mut contender_file = File::open("src/test_contender").unwrap();
+
+    let mut original = String::new();
+    let mut contender = String::new();
+
+    original_file.read_to_string(&mut original).unwrap();
+    contender_file.read_to_string(&mut contender).unwrap();
+
+    let diff = TextDiff::from_lines(&original, &contender);
+    for change in diff.iter_all_changes() {
+        let sign = match change.tag() {
+            ChangeTag::Delete => "---",
+            ChangeTag::Insert => "+++",
+            ChangeTag::Equal => " ",
+        };
+
+        print!("{}   {}", sign, change);
+    }
+
+    if original != contender {
+        eprintln!("NOPE");
+    } else {
+        eprintln!("GG");
     }
 }
